d4d527ec15a3c85ce1431a6d269542caf504dc46
Alex terentev <ih8-t@yandex.ru>
2022-08-24 16:31:31 +0300

    сделал одаптив под телефон

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,29 @@
-console.log('script work!');
+// $(document).ready(function() {
+
+//     $('js-burger').on('click', function(){
+//         if('isOpen') {
+//            $('.js-menu').slideUp();
+//            isOpen = true;
+//         } else {
+//             $('.js-menu').slideDown();
+//             isOpen = true;
+//         }
+//     })
+//     console.log('js-burger');
+// })
+
+
+$(document).ready(function() {
+    
+    let isOpen = false;
+  
+     $('.js-burger').on('click', function(){
+       if (isOpen) {
+        $('.js-menu').slideUp();
+        isOpen = false;
+      } else {
+         $('.js-menu').slideDown();
+     isOpen = true;
+      }
+    });
+})
